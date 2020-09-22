c145868ca2379ab2396d2143fcfa71871dbbbe20
Phantasyx <jordanhill999@yahoo.com>
2020-09-21 20:11:36 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: X
+ * Date: 4/20/2017
+ * Time: 7:09 PM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simon</title>
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="site.con.js"></script>
+</head>
+<body>
+
+</body>
+</html>
